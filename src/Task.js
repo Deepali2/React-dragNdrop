4cd5e4edb614fc3aa37ee1cc09eeb5f70bfaf5b0
Deepali2 <hdgarg@gmail.com>
2019-08-11 16:49:51 -0700

    controls where items can be dropped using type

--- src/Task.js
+++ src/Task.js
@@ -7,19 +7,26 @@ const Container = styled.div`
   border-radius: 40px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: ${props => (props.isDragging ? "lightgreen" : "white")};
+  background-color: ${props =>
+    props.isDisabled ? "lightgrey" : props.isDragging ? "lightgreen" : "white"};
 `;
 
 export default class Task extends Component {
   render() {
+    const isDragDisabled = this.props.task.id === "task-1";
     return (
-      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+      <Draggable
+        draggableId={this.props.task.id}
+        index={this.props.index}
+        isDragDisabled={isDragDisabled}
+      >
         {(provided, snapshot) => (
           <Container
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
+            isDragging={snapshot.isDragging}
           >
             {this.props.task.content}
           </Container>
