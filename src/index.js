0fddffd2c68d8cbb52598e9f4bbc1a87fb517b0e
Deepali2 <hdgarg@gmail.com>
2019-08-11 14:54:44 -0700

    adds styling while starting, during, and ending the drag

--- src/index.js
+++ src/index.js
@@ -8,7 +8,23 @@ import Column from "./Column";
 class App extends Component {
   state = initialData;
 
+  onDragStart = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.5s ease";
+  };
+
+  onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity}`;
+  };
+
   onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+
     const { destination, source, draggableId } = result;
     if (!destination) return;
     if (
@@ -36,7 +52,11 @@ class App extends Component {
 
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
